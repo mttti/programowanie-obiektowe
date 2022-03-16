b02726b6916833a90567b30ff996f4988adfa63c
mateusz.magiera <mateusz.magiera@PK3-05>
2022-03-16 09:37:25 +0100

    lab2

---
+++ lab-2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class
+}
